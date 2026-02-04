d7152a9e0149f3fcdcd0f687290179fa6168b4f2
fragkotseni <tsenifragko@myocu.oak.edu>
2026-02-04 10:27:00 -0600

    version 2
    -Completed needed components
    -Cleaned up project directory
    -Removed import statements that was no longer needed by project clean up

---
+++ todo-list-lab/src/components/TodoItem.js
@@ -0,0 +1,37 @@
+import Button from `./Button`;
+
+function ToDoItem({ todo, onToggle, onDelete}) {
+    //ToDo - object for each tasks(id, text, completed)
+    //onToggle - function taht needs to be called when checkbox is clicked
+    //onDelete - function to call when to Delete btn is clicked
+
+    return(
+
+        <div className="todo-item">
+            //Checkbox to toggle status
+            <input 
+            type="checkbox" 
+            checked={todo.completed} 
+            onChange={onToggle}
+            />
+
+            //Todo text with conditional strikethorugh styling if completed 
+            <span
+                style={{
+                    textDeclaration: todo.completed ? 'line-through' : 'none',
+                    marginLeft: '10px',
+                    marginRight: '10px'
+                }}
+            >
+            </span>
+
+            //Delete btn using out reusable btn component
+            <Button text="Delete" onClick={onDelete} />
+
+        </div>
+
+    );
+}
+
+
+export default ToDoItem;
